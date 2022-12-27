aff51965057ffe296d67492985106311c741c804
Alfi Maulana <alfi.maulana.f@gmail.com>
2022-12-27 14:58:55 +0700

    add test for `vol::device_list`

---
+++ test/device_test.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch_test_macros.hpp>
+#include <volume/device.hpp>
+
+TEST_CASE("list devices") {
+  const auto devices = vol::list_devices();
+  REQUIRE_FALSE(devices.empty());
+}
