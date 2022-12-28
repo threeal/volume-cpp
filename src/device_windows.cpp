94d111f4d96b337bbd3881d759d68ee7abe40521
Alfi Maulana <alfi.maulana.f@gmail.com>
2022-12-28 14:21:00 +0700

    modify if branch format

--- src/device_windows.cpp
+++ src/device_windows.cpp
@@ -5,25 +5,33 @@ namespace vol {
 
 res::ResultOr<Devices> list_input_devices() {
   auto com = utils::com_init();
-  if (!com.is_ok()) return res::Err("failed to initialize COM");
+  if (!com.is_ok()) {
+    return res::Err("failed to initialize COM");
+  }
   auto enumerator = com.create_device_enumerator();
-  if (!enumerator.is_ok())
+  if (!enumerator.is_ok()) {
     return res::Err("failed to create a device enumerator");
+  }
   auto devices = enumerator.enumerate_input_devices();
-  if (!devices.is_ok())
+  if (!devices.is_ok()) {
     return res::Err("failed to enumerate input devices");
+  }
   return Devices{};
 }
 
 res::ResultOr<Devices> list_output_devices() {
   auto com = utils::com_init();
-  if (!com.is_ok()) return res::Err("failed to initialize COM");
+  if (!com.is_ok()) {
+    return res::Err("failed to initialize COM");
+  }
   auto enumerator = com.create_device_enumerator();
-  if (!enumerator.is_ok())
+  if (!enumerator.is_ok()) {
     return res::Err("failed to create a device enumerator");
+  }
   auto devices = enumerator.enumerate_output_devices();
-  if (!devices.is_ok())
+  if (!devices.is_ok()) {
     return res::Err("failed to enumerate output devices");
+  }
   return Devices{};
 }
 
