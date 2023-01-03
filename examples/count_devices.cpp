ba8db57fce8f23f4edbcef596c4591c459805e1e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-03 11:01:39 +0700

    add example of counting audio input and output devices

---
+++ examples/count_devices.cpp
@@ -0,0 +1,18 @@
+#include <volume/device.hpp>
+#include <iostream>
+
+int main() {
+  const auto inputs = vol::list_input_devices();
+  if (inputs.is_err()) {
+    std::cerr << "failed to list input devices! " << inputs.unwrap_err() << std::endl;
+    return 1;
+  }
+  const auto outputs = vol::list_output_devices();
+  if (outputs.is_err()) {
+    std::cerr << "failed to list output devices! " << outputs.unwrap_err() << std::endl;
+    return 1;
+  }
+  std::cout << "input devices: " << inputs.unwrap().size() << std::endl;
+  std::cout << "output devices: " << outputs.unwrap().size() << std::endl;
+  return 0;
+}
